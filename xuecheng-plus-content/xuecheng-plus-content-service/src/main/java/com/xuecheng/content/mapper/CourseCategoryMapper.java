f635bb689eb8094e90f9236144c177c4d75d8698
荔枝味 <2090037702@qq.com>
2024-12-27 19:03:07 +0800

    提交课程查询接口定义代码

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseCategoryMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseCategoryMapper.java
@@ -1,8 +1,10 @@
 package com.xuecheng.content.mapper;
 
-import com.xuecheng.content.model.po.CourseCategory;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.model.po.CourseCategory;
 
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +14,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author itcast
  */
 public interface CourseCategoryMapper extends BaseMapper<CourseCategory> {
+
+    //使用递归查询分类
+    public List<CourseCategoryTreeDto> selectTreeNodes(String id);
 
 }
